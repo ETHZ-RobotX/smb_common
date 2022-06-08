b4ad8df12ec45bf13da84839212de1749c23f9b1
Johannes Pankert <Johannes.Pankert@mavt.ethz.ch>
2022-06-08 11:09:05 +0200

    add some hints and simplify structure for MPC tutorial

--- smb_mpc/src/SmbSystemDynamics.cpp
+++ smb_mpc/src/SmbSystemDynamics.cpp
@@ -5,27 +5,34 @@
 
 using namespace ocs2;
 
-namespace smb_mpc
-{
-  SmbSystemDynamics::SmbSystemDynamics(const std::string &modelName,
-                                       const std::string &modelFolder, bool recompileLibraries, bool verbose)
-  {
-    this->initialize(SmbDefinitions::STATE_DIM, SmbDefinitions::INPUT_DIM, modelName, modelFolder, recompileLibraries, verbose);
-  }
-  ad_vector_t SmbSystemDynamics::systemFlowMap(ocs2::ad_scalar_t time, const ocs2::ad_vector_t &state, const ocs2::ad_vector_t &input,
-                                               const ocs2::ad_vector_t &parameters) const
-  {
-    ad_vector_t stateDerivative = ad_vector_t::Zero(SmbDefinitions::STATE_DIM);
+namespace smb_mpc {
+SmbSystemDynamics::SmbSystemDynamics(const std::string &modelName,
+                                     const std::string &modelFolder,
+                                     bool recompileLibraries, bool verbose) {
+  this->initialize(SmbDefinitions::STATE_DIM, SmbDefinitions::INPUT_DIM,
+                   modelName, modelFolder, recompileLibraries, verbose);
+}
+ad_vector_t SmbSystemDynamics::systemFlowMap(
+    ocs2::ad_scalar_t time, const ocs2::ad_vector_t &state,
+    const ocs2::ad_vector_t &input, const ocs2::ad_vector_t &parameters) const {
 
-    using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
+  using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
+  using ad_vec3_t = Eigen::Matrix<ad_scalar_t, 3, 1>;
 
-    ad_scalar_t linearVelocity = SmbConversions::readLinVel(input);
-    ad_scalar_t omega = SmbConversions::readAngVel(input);
+  ad_vec3_t positionDerivative = ad_vec3_t::Zero();
+  ad_quat_t orientationDerivative;
+  orientationDerivative.coeffs().setZero();
 
-    ad_quat_t currentRotation = SmbConversions::readRotation(state);
+  ad_scalar_t v_x = SmbConversions::readLinVel(input);
+  ad_scalar_t omega_z = SmbConversions::readAngVel(input);
 
-    ///todo Compute state derivative here:
+  ad_vec3_t currentPosition = SmbConversions::readPosition(state);
+  ad_quat_t currentRotation = SmbConversions::readRotation(state);
 
-    return stateDerivative;
-  }
+  /// TODO: Compute positionDerivative and orientationDerivative here:
+
+  ad_vector_t stateDerivative = ad_vector_t::Zero(SmbDefinitions::STATE_DIM);
+  stateDerivative << positionDerivative, orientationDerivative.coeffs();
+  return stateDerivative;
+}
 } // namespace smb_mpc
--- smb_mpc/src/cost/SmbCost.cpp
+++ smb_mpc/src/cost/SmbCost.cpp
@@ -25,20 +25,33 @@ ad_vector_t SmbCost::costVectorFunction(ad_scalar_t time,
                                         const ad_vector_t &parameters) const {
 
   using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
+  using ad_mat_t = Eigen::Matrix<ad_scalar_t, -1, -1>;
+  using ad_angle_axis_t = Eigen::AngleAxis<ad_scalar_t>;
+  using ad_vec3_t = Eigen::Matrix<ad_scalar_t, 3, 1>;
 
   const ad_vector_t currentPosition = SmbConversions::readPosition(state);
   const ad_quat_t currentOrientation = SmbConversions::readRotation(state);
 
-  const ad_vector_t desiredPosition = SmbConversions::readPosition(parameters);
-  const ad_quat_t desiredOrientation = SmbConversions::readRotation(parameters);
+  ad_vector_t desiredPosition = SmbConversions::readPosition(parameters);
+  ad_quat_t desiredOrientation = SmbConversions::readRotation(parameters);
+
+  const ad_mat_t QPosition = QPosition_.cast<ad_scalar_t>();
+  const ad_mat_t QOrientation = QOrientation_.cast<ad_scalar_t>();
+  const ad_mat_t R = R_.cast<ad_scalar_t>();
 
   ad_vector_t positionError = ad_vector_t::Zero(3);
   ad_vector_t orientationError = ad_vector_t::Zero(3);
-  ad_vector_t totalCost(positionError.size() + orientationError.size() +
-                        input.size());
+  ad_vector_t inputError = ad_vector_t::Zero(2);
 
-  /// todo Compute totalCost here. You can use the weight matricies
-  /// QPosition_, QOrientation_ and R_
+  /// TODO: Compute cost terms positionError, orientationError, and inputError
+  /// here. For Task 2, overwrite currentPosition and currentOrientation with
+  /// the desired setpoint. For Task 3, keep the values set in L33-34 You can
+  /// use the weight matricies QPosition, QOrientation and R for Task 4.
+
+
+  ad_vector_t totalCost(positionError.size() + orientationError.size() +
+                        inputError.size());
+  totalCost << positionError, orientationError, inputError;
 
   return totalCost;
 }
@@ -48,6 +61,7 @@ SmbCost::getParameters(scalar_t time,
                        const TargetTrajectories &costDesiredTrajectory) const {
   const auto &desiredStateTrajectory = costDesiredTrajectory.stateTrajectory;
   const auto &desiredTimeTrajectory = costDesiredTrajectory.timeTrajectory;
+  int numPoses = desiredTimeTrajectory.size();
   Eigen::Vector3d referencePosition = Eigen::Vector3d::Zero();
   Eigen::Quaterniond referenceOrientation = Eigen::Quaterniond::Identity();
 
@@ -58,11 +72,13 @@ SmbCost::getParameters(scalar_t time,
     // Eigen::Quaterniond to get the reference quaternion and
     // SmbConversions::readPosition(desiredStateTrajectory[index]) -->
     // Eigen::Vector3d to get the reference position.
-    // desiredTimeTrajectory is an std::vector<double> of the reference timestamps.
+    // desiredTimeTrajectory is an std::vector<double> of the reference
+    // timestamps.
 
   } else { // desiredStateTrajectory.size() == 1, Do not change this
     referencePosition = SmbConversions::readPosition(desiredStateTrajectory[0]);
-    referenceOrientation = SmbConversions::readRotation(desiredStateTrajectory[0]);
+    referenceOrientation =
+        SmbConversions::readRotation(desiredStateTrajectory[0]);
   }
 
   vector_t reference(SmbDefinitions::STATE_DIM);
