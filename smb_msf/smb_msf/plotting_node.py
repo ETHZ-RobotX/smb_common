e890e7c756d0fc300b6f907880b128a1e95b9a9d
Juli <nubertj@ethz.ch>
2022-06-30 03:02:56 +0200

    added running subscriber, now only bias and state plotting missing

--- smb_msf/smb_msf/plotting_node.py
+++ smb_msf/smb_msf/plotting_node.py
@@ -1,7 +1,41 @@
+#!/usr/bin/env python3
+
+# Global
 import numpy
 import matplotlib
 import rospy
 
-print("Hello world!")
+# Custom
+from msf_core.msg import DoubleArrayStamped
+
 
 # TODO: Add a main that plots the trajectory and covariance of the estimation.
+
+class MsfPlotter:
+
+  def __init__(self):
+
+    self.state_topic_name = "/msf_core/state_out"
+
+    # ROS
+    rospy.init_node('MSF Publisher Node', anonymous=True)
+
+
+  def subscriber_callback(self, state_estimate):
+    print("Received state message.")
+
+
+  def start_plotting(self):
+
+    print("Starting plotting...")
+    rospy.Subscriber(self.state_topic_name, DoubleArrayStamped,
+                         self.subscriber_callback)
+
+    rospy.spin()
+
+
+
+if __name__ == "__main__":
+  
+  msf_plotter = MsfPlotter()
+  msf_plotter.start_plotting()
