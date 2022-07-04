12404aa35f606c2935556465a5c3055666dad4c4
Eytan Canzini <ethan.canzini@gmail.com>
2022-07-04 17:04:02 +0100

    Fixed MPC node

--- smb_mpc/src/SmbSystemDynamics.cpp
+++ smb_mpc/src/SmbSystemDynamics.cpp
@@ -1,38 +1,88 @@
 //
 // Created by johannes on 01.05.19.
 //
+// #include <smb_mpc/SmbSystemDynamics.h>
+
+// using namespace ocs2;
+
+// namespace smb_mpc {
+// SmbSystemDynamics::SmbSystemDynamics(const std::string &modelName,
+//                                      const std::string &modelFolder,
+//                                      bool recompileLibraries, bool verbose) {
+//   this->initialize(SmbDefinitions::STATE_DIM, SmbDefinitions::INPUT_DIM,
+//                    modelName, modelFolder, recompileLibraries, verbose);
+// }
+// ad_vector_t SmbSystemDynamics::systemFlowMap(
+//     ocs2::ad_scalar_t time, const ocs2::ad_vector_t &state,
+//     const ocs2::ad_vector_t &input, const ocs2::ad_vector_t &parameters) const {
+
+//   using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
+//   using ad_vec3_t = Eigen::Matrix<ad_scalar_t, 3, 1>;
+
+//   ad_vec3_t positionDerivative = ad_vec3_t::Zero();
+//   ad_quat_t orientationDerivative;
+//   orientationDerivative.coeffs().setZero();
+
+//   ad_scalar_t v_x = SmbConversions::readLinVel(input);
+//   ad_scalar_t omega_z = SmbConversions::readAngVel(input);
+
+//   ad_vec3_t currentPosition = SmbConversions::readPosition(state);
+//   ad_quat_t currentRotation = SmbConversions::readRotation(state);
+
+//   // Calculate the position derivative RELATIVE TO THE BASE FRAME OF THE WORLD
+//   ad_vec3_t linearVelocity = ad_vec3_t::Zero();
+//   linearVelocity[0] = v_x;
+//   positionDerivative = currentRotation*linearVelocity;
+
+//   // Calculate the orientation derivative 
+//   ad_quat_t deltaRotation;
+//   deltaRotation.w() = 0;
+//   deltaRotation.x() = 0;
+//   deltaRotation.y() = 0;
+//   deltaRotation.z() = omega_z/2;
+//   orientationDerivative = currentRotation*deltaRotation;
+
+//   ad_vector_t stateDerivative = ad_vector_t::Zero(SmbDefinitions::STATE_DIM);
+//   stateDerivative << positionDerivative, orientationDerivative.coeffs();
+//   return stateDerivative;
+// }
+// } // namespace smb_mpc
 #include <smb_mpc/SmbSystemDynamics.h>
 
 using namespace ocs2;
 
-namespace smb_mpc {
-SmbSystemDynamics::SmbSystemDynamics(const std::string &modelName,
-                                     const std::string &modelFolder,
-                                     bool recompileLibraries, bool verbose) {
-  this->initialize(SmbDefinitions::STATE_DIM, SmbDefinitions::INPUT_DIM,
-                   modelName, modelFolder, recompileLibraries, verbose);
-}
-ad_vector_t SmbSystemDynamics::systemFlowMap(
-    ocs2::ad_scalar_t time, const ocs2::ad_vector_t &state,
-    const ocs2::ad_vector_t &input, const ocs2::ad_vector_t &parameters) const {
+namespace smb_mpc
+{
+  SmbSystemDynamics::SmbSystemDynamics(const std::string &modelName,
+                                       const std::string &modelFolder, bool recompileLibraries, bool verbose)
+  {
+    this->initialize(SmbDefinitions::STATE_DIM, SmbDefinitions::INPUT_DIM, modelName, modelFolder, recompileLibraries, verbose);
+  }
+  ad_vector_t SmbSystemDynamics::systemFlowMap(ocs2::ad_scalar_t time, const ocs2::ad_vector_t &state, const ocs2::ad_vector_t &input,
+                                               const ocs2::ad_vector_t &parameters) const
+  {
+    ad_vector_t stateDerivative(SmbDefinitions::STATE_DIM);
 
-  using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
-  using ad_vec3_t = Eigen::Matrix<ad_scalar_t, 3, 1>;
+    using ad_quat_t = Eigen::Quaternion<ad_scalar_t>;
 
-  ad_vec3_t positionDerivative = ad_vec3_t::Zero();
-  ad_quat_t orientationDerivative;
-  orientationDerivative.coeffs().setZero();
+    ad_scalar_t linearVelocity = SmbConversions::readLinVel(input);
+    ad_scalar_t omega = SmbConversions::readAngVel(input);
 
-  ad_scalar_t v_x = SmbConversions::readLinVel(input);
-  ad_scalar_t omega_z = SmbConversions::readAngVel(input);
+    ad_quat_t currentRotation = SmbConversions::readRotation(state);
 
-  ad_vec3_t currentPosition = SmbConversions::readPosition(state);
-  ad_quat_t currentRotation = SmbConversions::readRotation(state);
+    // derivative of orientation quaternion: https://fgiesen.wordpress.com/2012/08/24/quaternion-differentiation/
+    ad_quat_t deltaRotation;
+    deltaRotation.w() = 0;
+    deltaRotation.x() = 0;
+    deltaRotation.y() = 0;
+    deltaRotation.z() = omega / 2;
+    stateDerivative.tail(4) = (currentRotation * deltaRotation).coeffs();
 
-  /// TODO: Compute positionDerivative and orientationDerivative here:
+    ad_vector_t linearVelocityVector = ad_vector_t::Zero(3);
+    linearVelocityVector[0] = linearVelocity;
+    stateDerivative.head(3) = currentRotation * linearVelocityVector;
+    // stateDerivative[2] = 0 // force derivative in z direction to 0
 
-  ad_vector_t stateDerivative = ad_vector_t::Zero(SmbDefinitions::STATE_DIM);
-  stateDerivative << positionDerivative, orientationDerivative.coeffs();
-  return stateDerivative;
-}
+    return stateDerivative;
+  }
 } // namespace smb_mpc
