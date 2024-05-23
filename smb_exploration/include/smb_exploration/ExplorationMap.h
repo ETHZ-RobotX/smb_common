77cd5d6ea2dd6008db3ff3da8aed51081c4cd110
changan <chencha@student.ethz.ch>
2024-05-23 16:00:07 +0200

    interfacing lidar scan with exploration

--- smb_exploration/include/smb_exploration/ExplorationMap.h
+++ smb_exploration/include/smb_exploration/ExplorationMap.h
@@ -5,6 +5,7 @@
 #include <costmap_2d/costmap_2d_ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
+#include <laser_geometry/laser_geometry.h>
 
 
 namespace smb_exploration {
@@ -20,6 +21,7 @@ private:
 
     // publisher
     ros::Publisher occ_pub_;
+    ros::Publisher pcd_pub_;
 
     // costmap
     costmap_2d::Costmap2DROS* costmap2d_ros_;
@@ -28,9 +30,26 @@ private:
     tf2_ros::Buffer tfBuffer_;
     tf2_ros::TransformListener tfListener_;  
 
+    laser_geometry::LaserProjection projector_;  // Used to project laser scans into point clouds
+
+    // frames
+    std::string global_frame_;
+
+    // map params
+    int min_x_;
+    int min_y_;
+    int max_x_;
+    int max_y_;
+    int update_block_size_{10};
+    double resolution_;
+
     // callbacks
     void scanCallback(const sensor_msgs::LaserScanConstPtr& scan);
-    
+
+    bool shouldUpdateBounds(const float min_x,const float min_y, const float max_x, const float max_y);
+
+    void updateBounds(const float min_x,const float min_y, const float max_x, const float max_y);
+
 public:
     ExplorationMap(ros::NodeHandle nh);
 };
