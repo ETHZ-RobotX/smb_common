61a82ff3639e502ac6eb1def4711de19da5d6b1e
Magdalena Yordanova <maggi.jordanova@gmail.com>
2024-07-03 14:40:34 +0000

    adjust the subscriber for object_detection_saver

--- team6_stack/scripts/object_detection_saver.py
+++ team6_stack/scripts/object_detection_saver.py
@@ -15,6 +15,7 @@ def callback(msg):
     for info in msg.info:
         info = {
             'class_id': info.class_id,
+            # TODO: Add 'id' if it's not always 0
             'x': info.position.x, # in camera frame
             'y': info.position.y, # in camera frame
             'z': info.position.z # in camera frame
@@ -38,8 +39,8 @@ def callback(msg):
             for info in infos:
                 writer.writerow([info['class_id'], info['x'], info['y'], info['z']])
     
-def object_detection_listener():
-    rospy.init_node('team6_stack', anonymous=True)
+def object_detection_saver():
+    rospy.init_node('object_detection_saver', anonymous=True)
 
     global timestr
     
@@ -57,11 +58,12 @@ def object_detection_listener():
             writer = csv.writer(file)
             writer.writerow(['class_id', 'x', 'y', 'z'])
 
-    # Subscribe to the /object_detector/detection_info topic
-    rospy.Subscriber('/object_detector/detection_info', ObjectDetectionInfoArray, callback)
+    # Subscribe to topics
+    #rospy.Subscriber('/object_detector/detection_info', ObjectDetectionInfoArray, callback)
+    rospy.Subscriber('/object_inspector/unique_artifacts', ObjectDetectionInfoArray, callback)
     
     # Keep the script running
     rospy.spin()
 
 if __name__ == '__main__':
-    object_detection_listener()
+    object_detection_saver()
